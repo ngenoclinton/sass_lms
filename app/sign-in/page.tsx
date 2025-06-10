3614b1bd1095c7ccaee36a24621d9a174c9fe652
ngenoclinton <clintonkipkoechngeno@gmail.com>
2025-06-10 19:59:16 +0300

    first commit

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h2 className='text-3xl text-yellow-700'>Sign -in to your account </h2>
+    </div>
+  )
+}
+
+export default page
